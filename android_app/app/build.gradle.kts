324a7c26b20f0aeb42ca8834df2a1954a4b1e5aa
TPoll <148467231+TPollTech@users.noreply.github.com>
2026-07-07 11:43:29 -0300

    release: v1.5.0 - Login com Google via Firebase

--- android_app/app/build.gradle.kts
+++ android_app/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.tpoll.scanner"
         minSdk = 26
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.4.2"
+        versionCode = 10
+        versionName = "1.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
