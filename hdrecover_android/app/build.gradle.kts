b304c1f524deeab33e7faefff10cf0a84c9e54cb
TPoll <148467231+TPollTech@users.noreply.github.com>
2026-07-19 20:48:27 -0300

    build(hdrecover): configurar aplicativo Android dedicado

---
+++ hdrecover_android/app/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.tpoll.hdrecover"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.tpoll.hdrecover"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    packaging {
+        resources.excludes += "/META-INF/{AL2.0,LGPL2.1}"
+    }
+}
