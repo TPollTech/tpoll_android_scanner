590061d7dc8fcc3e5f65dd639f8be2eff25d7df0
TPoll <148467231+TPollTech@users.noreply.github.com>
2026-07-20 07:15:31 -0300

    Bump version to 1.7.0

--- android_app/app/build.gradle.kts
+++ android_app/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.tpoll.scanner"
         minSdk = 26
         targetSdk = 34
-        versionCode = 11
-        versionName = "1.6.0"
+        versionCode = 12
+        versionName = "1.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
